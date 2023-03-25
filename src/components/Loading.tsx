e3ae64c897a8a31c53b668ac50817a5972110012
keitomatsuri <keitomatsuri@gmail.com>
2023-03-25 14:28:05 +0900

    ローディング文言追加

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,9 @@
 export default function Loading() {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="px-6 py-4">
+      <div className="px-6 py-4 text-center">
         <p>AIが考え中です。のんびりお待ちください。</p>
+        <p>1分以上かかる場合はAIが寝ているので、時間をおいて再度お試してください。</p>
       </div>
       <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
     </div>
