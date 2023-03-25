0994724159dca447de4d520bdce59242f95fbfd4
keitomatsuri <keitomatsuri@gmail.com>
2023-03-25 15:43:12 +0900

    ga対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import Head from 'next/head'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "@/lib/gtag";
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouterChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouterChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouterChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
@@ -23,6 +38,23 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="twitter:url" content={`${process.env.NEXT_PUBLIC_SITE_URL}`} />
         <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_SITE_URL}/og_image.png`} />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
